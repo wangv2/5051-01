09283dce632d48195045d04d835e30b64b0d5b6e
Wang <wangv2@seattleu.edu>
2018-05-10 18:39:39 -0700

    changed student index

---
+++ 5051/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(string id=null)
+        {
+            var myStudentID = "";
+            if (!string.IsNullOrEmpty(id))
+            {
+                myStudentID = id;
+            }
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
